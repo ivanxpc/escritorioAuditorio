f9c263e380fc0a8eee27d4d58fdbc2551957abe9
OmarRz231 <126817605+OmarRz231@users.noreply.github.com>
2023-03-24 00:09:00 -0600

    AVANCE 2: Agregue una clase y librerias para las conexiones sql. Hecho por Omar

---
+++ src/main/java/com/example/escritorioauditorio/ConexionBD.java
@@ -0,0 +1,21 @@
+package com.example.escritorioauditorio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBD {
+
+    private static Connection c;
+
+    public static Connection getConexion(){
+        try {
+            if (c==null) {
+                String url = "jdbc:mysql://localhost:3306/auditorio";
+                c = DriverManager.getConnection(url, "root", "12345");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return c;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.escritorioauditorio {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.escritorioauditorio to javafx.fxml;
