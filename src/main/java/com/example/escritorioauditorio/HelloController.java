d4896e5d7a4b4a06933beb4110c26a91fb5da59a
Alejandro121102 <ch200110785@chapala.tecmm.edu.mx>
2023-03-24 21:46:20 -0600

    Interfaz Fechas

--- src/main/java/com/example/escritorioauditorio/HelloController.java
+++ src/main/java/com/example/escritorioauditorio/HelloController.java
@@ -1,18 +1,47 @@
 package com.example.escritorioauditorio;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    //Fechas Disponibles
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
+    private TextField Nota;
     @FXML
-    private Button gael;
+    private DatePicker Calendario;
+    @FXML
+    private CheckBox Sillas;
+    @FXML
+    private CheckBox Tablones;
+    @FXML
+    private CheckBox Mesas;
+    @FXML
+    private CheckBox Microfonos;
+    @FXML
+    private CheckBox Podium;
+    @FXML
+    private CheckBox Baños;
+    @FXML
+    private CheckBox Agua;
+    @FXML
+    private CheckBox Manteleria;
+    @FXML
+    private CheckBox Personas1;
+    @FXML
+    private CheckBox Personas2;
+    @FXML
+    private CheckBox Personas3;
+    @FXML
+    private CheckBox Personas4;
+    @FXML
+    private CheckBox Horario1;
+    @FXML
+    private CheckBox Horario2;
+    @FXML
+    private CheckBox Horario3;
+    @FXML
+    private CheckBox Horario4;
+
 }
