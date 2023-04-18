8bcc6527fe5ee35882616cef744ed6f11911aeb1
OmarRz231 <126817605+OmarRz231@users.noreply.github.com>
2023-04-17 19:31:11 -0600

    AVANCE 17/04/2023: Se modifico el metodo de login

--- src/main/java/com/example/escritorioauditorio/loginController.java
+++ src/main/java/com/example/escritorioauditorio/loginController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.util.regex.*;
 
 public class loginController {
 
@@ -19,31 +20,41 @@ public class loginController {
 
     @FXML
     public void Aceptar(ActionEvent evt)throws Exception{
-        ConexionLogin BD = new ConexionLogin();
-        String Val_PASSWORD = contraseña.getText();
-        String Val_USER= usuario.getText();
-        BD.ConectarBasedeDatos();
 
-        String SQL = "SELECT id FROM usuarios" +
-                " WHERE nombre='"+Val_USER+"' AND password='"+Val_PASSWORD+"'";
-        BD.resultado = BD.sentencia.executeQuery(SQL);
+        Pattern patron = Pattern.compile("[A-Za-z0-9_.]*||(.*@[g][m][a][i][l][.][c][o][m])||" +
+                                         "(.*@[c][h][a][p][a][l][a][.][t][e][c][m][m][.][e][d][u][.][m][x])");
+        Matcher mat = patron.matcher(usuario.getText());
 
-        if(BD.resultado.next()){
-            Stage s = (Stage) aceptar.getScene().getWindow();
-            s.close();
+        if(mat.matches()) {
+            ConexionLogin BD = new ConexionLogin();
+            String Val_PASSWORD = contraseña.getText();
+            String Val_USER= usuario.getText();
+            BD.ConectarBasedeDatos();
 
-            Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-            Scene escena = new Scene(loader.load());
-            stage.setScene(escena);
-            stage.show();
-        }else{
-            Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("errorLogin.fxml"));
-            stage.setTitle("ERROR");
-            Scene escena = new Scene(loader.load());
-            stage.setScene(escena);
-            stage.show();
+            //String SQL = "SELECT id FROM usuarios" +
+              //      " WHERE nombre='"+Val_USER+"' AND password='"+Val_PASSWORD+"'";
+            String SQL = "SELECT id FROM usuarios" +
+                    " WHERE nombre='"+Val_USER+"'OR correo='"+Val_USER+"' AND password='"+Val_PASSWORD+"'";
+
+            BD.resultado = BD.sentencia.executeQuery(SQL);
+
+            if (BD.resultado.next()) {
+                Stage s = (Stage) aceptar.getScene().getWindow();
+                s.close();
+
+                Stage stage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+                Scene escena = new Scene(loader.load());
+                stage.setScene(escena);
+                stage.show();
+            } else {
+                Stage stage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("errorLogin.fxml"));
+                stage.setTitle("ERROR");
+                Scene escena = new Scene(loader.load());
+                stage.setScene(escena);
+                stage.show();
+            }
         }
     }
 }
