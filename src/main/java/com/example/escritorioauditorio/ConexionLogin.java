d5d6806fbfcf983f4ab7309b65db69d734ffb1d8
Alejandro121102 <ch200110785@chapala.tecmm.edu.mx>
2023-04-18 20:52:33 -0600

    Diseño

--- src/main/java/com/example/escritorioauditorio/ConexionLogin.java
+++ src/main/java/com/example/escritorioauditorio/ConexionLogin.java
@@ -15,7 +15,7 @@ public class ConexionLogin{
     public void ConectarBasedeDatos() {
         try {
             final String url_bd = "jdbc:mysql://localhost:3306/auditorio";
-            conexion = DriverManager.getConnection(url_bd, "root", "");
+            conexion = DriverManager.getConnection(url_bd, "root", "12345");
             sentencia = conexion.createStatement();
         } catch (SQLException ex) {
             System.out.println("NO SE PUDO HACER LA CONEXION");
