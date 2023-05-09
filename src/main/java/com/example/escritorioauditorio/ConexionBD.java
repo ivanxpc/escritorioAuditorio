2af39e43a2bf8c61621150c5500ea3c60e1f895a
IVAN ALEJANDRO PADILLA CORDOVA <nalaivan123@hotmail.com>
2023-05-08 21:38:09 -0600

    Conexion al servidor del Tec 2.0

--- src/main/java/com/example/escritorioauditorio/ConexionBD.java
+++ src/main/java/com/example/escritorioauditorio/ConexionBD.java
@@ -9,6 +9,7 @@ public class ConexionBD {
         try {
             if (c==null) {
                 //Original
+                //Esto es la conexion
                // String url = "jdbc:mysql://localhost:3306/auditorio";
                 String url = "jdbc:mysql://65.99.252.253:3306/eduwitco_auditorio";
                 //Paso 1 = Crear una conexion;
